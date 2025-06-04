fea487c9a5cbde409d0725a48beecbc4488dbd1d
NIBURP <121443417+xxNIBURPxx@users.noreply.github.com>
2025-06-04 05:17:57 -0700

    Update SharePointOnline-Install.ps1

--- PowerShell/Modules/SharePointOnline-Install.ps1
+++ PowerShell/Modules/SharePointOnline-Install.ps1
@@ -1,2 +1,5 @@
 # This script installs the SharePoint Online Management Shell module.
 Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+# Import Module
+Import-Module Microsoft.Online.SharePoint.PowerShell
